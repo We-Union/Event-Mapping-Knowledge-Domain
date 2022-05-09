9bc722f04bb65a462eff8133db65daf1fc2ef5c1
LSTM-Kirigaya <1193466151@qq.com>
2022-05-09 14:20:12 +0800

    update

--- load_neo4j.py
+++ load_neo4j.py
@@ -10,9 +10,3 @@ with open(LOG_CONFIG_FILE, "r", encoding="utf-8") as fp:
 g = Graph(**c)
 event_graph = EventGraph(g)
 event_graph.load_data("./data/data_step2.json")
-
-# node1 = Node("Person", name="p1")
-# node2 = Node("Person", name="p2")
-
-# g.merge(node1, "Person", "name")
-# g.merge(node2, "Person", "name")
